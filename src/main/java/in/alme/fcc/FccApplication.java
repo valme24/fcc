a6496b548d93956d28ca72a3a40274a4cda89562
vanshgoyalatgithub <vansh.goel.0403@gmail.com>
2024-07-01 16:26:34 +0530

    adding .env file to protec credentials

--- src/main/java/in/alme/fcc/FccApplication.java
+++ src/main/java/in/alme/fcc/FccApplication.java
@@ -2,12 +2,18 @@ package in.alme.fcc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FccApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FccApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FccApplication.class, args);
+    }
+    @GetMapping
+    public String getRoot(){
+        return "Hello,World";
+    }
 }
